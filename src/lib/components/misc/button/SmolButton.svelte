4c7eb3d1708880a419d55483402cbeb82917314d
the-nepodarok <kosov.a@list.ru>
2023-11-17 22:26:32 +0300

    new component: smol button

---
+++ src/lib/components/misc/button/SmolButton.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let className = 'smol-button';
+    export let title = 'кнопка';
+    export let href;
+</script>
+
+<a class="{className}" {href}>
+    {title}
+</a>
+
+<style>
+    .smol-button {
+        align-self: flex-end;
+        max-width: 125px;
+        padding: 5px 20px;
+        font-size: 14px;
+        text-align: center;
+        text-transform: uppercase;
+        background-color: #D97544;
+        color: #FFFFFF;
+    }
+</style>
