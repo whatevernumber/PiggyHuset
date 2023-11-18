985744211e70ef44b37a35e866f0dda455e8276c
the-nepodarok <kosov.a@list.ru>
2023-11-18 21:43:22 +0300

    introducing three new pages: news, looking-for-home and graduates

---
+++ src/routes/graduates/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import CatalogPage from "$lib/components/pages/CatalogPage.svelte";
+
+    const data = [
+        {
+            title: 'Финик',
+            text: 'Переехал в Мурманск',
+            image: 'IMG_0695',
+            type: 'graduate',
+        },
+        {
+            title: 'Шанс',
+            text: 'На радуге',
+            image: 'FqAKtTrJP58',
+            type: 'graduate',
+        },
+        {
+            title: 'Йогурт',
+            text: 'Более ста фотографий из нового дома со всех сторон в карточке',
+            image: 'yoghurt',
+            type: 'graduate',
+        },
+        {
+            title: 'Эй',
+            text: 'Нашёл дом',
+            image: 'in-the-farm',
+            type: 'graduate',
+        },
+    ];
+</script>
+
+<CatalogPage {data} />
---
+++ src/routes/looking-for-home/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import CatalogPage from "$lib/components/pages/CatalogPage.svelte";
+
+    const data = [
+        {
+            title: 'Свиник',
+            type: 'ready',
+        },
+        {
+            title: 'Блиник',
+            type: 'ready',
+        },
+        {
+            title: 'Пиник',
+            type: 'ready',
+        },
+    ];
+</script>
+
+<CatalogPage pageTitle="Ищут дом" {data} />
