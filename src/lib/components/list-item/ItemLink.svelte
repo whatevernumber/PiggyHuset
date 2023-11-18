c3618108feeea437fae0b5e804ab26dc5d9081ae
whatevernumber <achford@mail.ru>
2023-11-18 17:02:58 +0300

    Fixed ItemLink component: changed margin padding and box-shadow values. Wrapped each list element into link.

--- src/lib/components/list-item/ItemLink.svelte
+++ src/lib/components/list-item/ItemLink.svelte
@@ -5,27 +5,27 @@
     let background_icon_url = "url('src/lib/img/" + icon + "')";
 </script>
 
-<li class="option-item" style="--background_url: {background_icon_url}">
-    <a class="option-item-link" {href}>
+<a class="option-item-link" {href}>
+    <li class="option-item" style="--background_url: {background_icon_url}">
         {text}
-    </a>
-</li>
+    </li>
+</a>
 
 <style>
     .option-item {
         position: relative;
-        padding: 17px 80px;
+        margin-left: 60px;
+        padding: 17px 50px;
         background-color: #FFFFFF;
         text-align: center;
         /*border-radius: 50px;*/
-        box-shadow: 1px 1px 15px rgba(0, 0, 0, 0.58);
     }
 
     .option-item::before {
         content: '';
         position: absolute;
         top: 0;
-        left: 0;
+        left: -60px;
         width: 60px;
         height: 60px;
         background-image: var(--background_url);
@@ -47,5 +47,7 @@
         font-size: 18px;
         font-weight: bold;
         color: #88AA4D;
+        box-shadow: 1px 1px 15px rgba(0, 0, 0, 0.58);
     }
 </style>
+
