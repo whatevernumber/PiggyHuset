22aa1160d2a6734e124704fe6a81779b5622dbc5
the-nepodarok <kosov.a@list.ru>
2023-11-25 20:39:24 +0300

    new page: turn-in form

---
+++ src/routes/turn-in/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import TurnInForm from "$lib/components/forms/TurnInForm.svelte";
+</script>
+
+<section class="container">
+    <TurnInForm />
+</section>
+
+<style>
+    .container {
+        min-height: 100%;
+        background: url("/img/main-bg.png") no-repeat;
+        background-size: cover;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
