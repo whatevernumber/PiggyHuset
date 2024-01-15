83ad2baad51c1b982af1d1a678c904b0ee2cf458
the-nepodarok <kosov.a@list.ru>
2024-01-15 22:12:15 +0300

    bringing new-post form to life

---
+++ src/lib/components/forms/NewPostForm.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Form from "$lib/components/forms/Form.svelte";
+
+    export let title;
+
+    const scheme = {
+        title: title || 'Новая публикация',
+        endpoint: '/articles/type/2',
+        fields: [
+            {
+                label: 'Заголовок публикации',
+                name: 'title'
+            },
+            {
+                label: 'Текст публикации',
+                name: 'text',
+                type: 'textarea',
+                required: true,
+                placeholder: 'Расскажите новость'
+            }
+        ],
+        files: {
+            file_input: true,
+            multiple: true
+        }
+    }
+</script>
+
+<Form {scheme} />
