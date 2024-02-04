fd44f69e8f477f74fc59ad9843005b94c33630fa
the-nepodarok <kosov.a@list.ru>
2024-02-04 20:26:28 +0300

    csp configuration

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,15 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		// Protection from XSS
+		csp: {
+			mode: 'auto',
+			directives: {
+				'base-uri': ['self'],
+				// 'img-src': ['self', 'backend domain'],
+				'script-src': ['self']
+			}
+		},
 		env: {
 			publicPrefix: '_'
 		}
