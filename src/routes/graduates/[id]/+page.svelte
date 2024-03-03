f477a660d4841e31d39c6886cf97fd9d1a17c0b3
the-nepodarok <kosov.a@list.ru>
2024-03-03 21:01:15 +0300

    switching status is back

--- src/routes/graduates/[id]/+page.svelte
+++ src/routes/graduates/[id]/+page.svelte
@@ -1,18 +1,16 @@
 <script>
 	import Article from '$lib/components/articles/Article.svelte';
 	import PigProfile from '$lib/components/cards/pig-profile-card/PigProfile.svelte';
-	import { redirect } from '$lib/components/utils/func.js';
-	import { _ADMIN_FLAG, _REMOTE_SERVER, _REST_STORAGE_KEY } from '$env/static/public';
-	import { onMount } from 'svelte';
+	import {closeModal, include_auth, redirect} from '$lib/components/utils/func.js';
+	import {_ADMIN_FLAG, _REMOTE_SERVER, _REST_STORAGE_KEY} from '$env/static/public';
+	import {onMount} from 'svelte';
 	import Overlay from '$lib/components/misc/overlay/Overlay.svelte';
 	import ModalOkay from '$lib/components/misc/modal/ModalOkay.svelte';
-	import { closeModal, include_auth } from '$lib/components/utils/func.js';
 
 	export let data;
 
 	const type = 'ready';
 	const pig = data.pig;
-	let pig_id = pig.id;
 	const pic = pig.main_photo;
 	const age = pig.age;
 	const graduated = pig.status_id;
@@ -28,6 +26,19 @@
 	let pig_status_id = pig.status ? pig.status.id : '';
 	let header = pig.name + ' ' + pig.status.text;
 	let status; // для отображения картинки статуса выпусника;
+
+	switch (graduated) {
+		case 2:
+			break;
+		case 3:
+			status = 'rainbow';
+			break;
+		case 4:
+			status = 'taken';
+			break;
+		default:
+			header;
+	}
 
 	let admin = false;
 
@@ -58,7 +69,7 @@
 	}
 
 	async function graduatePig (value) {
-		const res = await fetch(_REMOTE_SERVER + '/pigs/graduate/' + pig_id + '/' + value, {
+		const res = await fetch(_REMOTE_SERVER + '/pigs/graduate/' + pig.id + '/' + value, {
 			method: 'PATCH',
 			headers: {
 				'Authorization': include_auth(_REST_STORAGE_KEY)
@@ -83,7 +94,7 @@
 </svelte:head>
 
 <Article {date} {text} photos="{pig.photos}" type="graduate">
-	<PigProfile {city} {overseer} {graduated} {pig_sex} {pig_status} {pig_status_id} {taken} {rainbow} {pic} {header} {age} {type} {redirect_to_edit} {admin} bind:modal_opened={modal_opened} bind:status_value={status_value} />
+	<PigProfile {city} {overseer} {graduated} {graduation_date} {pig_sex} {pig_status} {pig_status_id} {status} {taken} {rainbow} {pic} {header} {age} {type} {redirect_to_edit} {admin} bind:modal_opened={modal_opened} bind:status_value={status_value} />
 </Article>
 
 {#if modal_opened}
