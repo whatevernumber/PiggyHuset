c4c1ff082eac971235fc56d4aef661517332a442
the-nepodarok <kosov.a@list.ru>
2024-03-05 19:28:09 +0300

    303 redirects

--- src/routes/graduates/[id]/+page.js
+++ src/routes/graduates/[id]/+page.js
@@ -1,9 +1,15 @@
 import {_REMOTE_SERVER} from "$env/static/public";
+import {redirect} from "@sveltejs/kit";
 
 export async function load({ fetch, params }) {
 
 	const res = await fetch(`${_REMOTE_SERVER}/pigs/${params.id}?expand=photos,overseer,city,status`);
-	const data = res.json();
+	const data = await res.json();
+
+
+	if (data.status.value !== 'graduated') {
+		throw redirect(303, `/looking-for-home/${params.id}`);
+	}
 
 	return {
 		pig: data,
