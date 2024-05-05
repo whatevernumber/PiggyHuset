c3aed4c1a28654f5f2be6ed5c91cde2471e19522
the-nepodarok <kosov.a@list.ru>
2024-05-05 22:39:19 +0300

    support for SvelteKit 2 (why tho)

--- src/routes/articles/+page.js
+++ src/routes/articles/+page.js
@@ -3,7 +3,7 @@ import {_REMOTE_SERVER} from "$env/static/public";
 export async function load({ fetch }) {
 
     const res = await fetch(`${_REMOTE_SERVER}/articles/type/1`);
-    const data = res.json();
+    const data = await res.json();
 
     return {
         articles: data,
--- src/routes/news/+page.js
+++ src/routes/news/+page.js
@@ -3,7 +3,7 @@ import {_REMOTE_SERVER} from "$env/static/public";
 export async function load({ fetch }) {
 
     const res = await fetch(`${_REMOTE_SERVER}/articles/type/2`);
-    const data = res.json();
+    const data = await res.json();
 
     return {
         news: data,
