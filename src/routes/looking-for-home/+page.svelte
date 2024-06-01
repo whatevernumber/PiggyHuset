477013d8bc70e930b60732f8e2c73d9cf4fe1e9f
whatevernumber <achford@mail.ru>
2024-06-01 20:44:43 +0300

    Revert "widgets, metrics & authors"

--- src/routes/looking-for-home/+page.svelte
+++ src/routes/looking-for-home/+page.svelte
@@ -1,6 +1,5 @@
 <script>
     import CatalogPage from "$lib/components/pages/CatalogPage.svelte";
-	import VKCommunityMessages from "$lib/components/widgets/VKCommunityMessages.svelte";
 
 	export let data;
 	const pigs = data.pigs;
@@ -11,4 +10,3 @@
 </svelte:head>
 
 <CatalogPage category="looking-for-home" page_title="Ищут дом" data="{pigs}" type="pig" />
-<VKCommunityMessages />
