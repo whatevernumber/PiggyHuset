5a35f330a3ecd67d9eb6db4a5c397ae085eea5b5
the-nepodarok <kosov.a@list.ru>
2024-11-16 16:08:24 +0300

    cookies banner added

---
+++ src/lib/components/banner/Cookies.svelte
@@ -0,0 +1,83 @@
+<script>
+	import { browser } from '$app/environment';
+
+	const hideCookies = function() {
+		if (browser) {
+			localStorage.setItem('hide-cookies', '1');
+			document.querySelector('.cookies').remove();
+		}
+	};
+</script>
+
+<div class="cookies">
+	<img src="/img/cookies.png" alt="Свинка с печенькой" class="cookies-image" width="100" height="100">
+	<div>
+		<h3 class="cookies-header">У нас свои печеньки!</h3>
+		<p class="cookies-text">Мы тоже используем cookies.</p>
+		<p class="cookies-text">Они помогают нам анализировать работу сайта и улучшать ваш опыт.</p>
+	</div>
+	<button class="cookies-close-button" on:click={hideCookies}></button>
+</div>
+
+<style>
+	.cookies {
+        display: flex;
+        min-width: 25vw;
+        max-width: 30vw;
+        height: 120px;
+		padding: 0 10px;
+
+		position: fixed;
+		bottom: 65px;
+        left: 0;
+        right: 0;
+		margin: 0 auto;
+
+        justify-content: center;
+        align-items: center;
+		column-gap: 15px;
+
+        background-color: aliceblue;
+        font-size: 14px;
+
+		opacity: 0.7;
+		transition-duration: 0.15s;
+        z-index: 10;
+	}
+
+	.cookies:hover {
+        opacity: 1;
+        background-color: #FFFFFF;
+		box-shadow: 1px 1px 15px #adadad;
+        transition-duration: 0.15s;
+    }
+
+	.cookies-close-button {
+        width: 40px;
+		height: 30px;
+		position: absolute;
+		right: 15px;
+		top: 15px;
+		background: url("/img/svg/close-ok.svg") no-repeat 50%;
+		border: none;
+		cursor: pointer;
+	}
+
+	.cookies-close-button:hover {
+		background-image: url("/img/svg/ok.svg");
+        /*border: 1px solid #e1edce;*/
+	}
+
+    @media (max-width: 1001px) {
+		.cookies {
+            bottom: 5px;
+            max-width: 95vw;
+			padding: 5px;
+
+			font-size: 15px;
+
+			opacity: 1;
+			box-shadow: none;
+        }
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,20 @@
 
 import {page} from "$app/stores";
 import Header from "$lib/components/header/Header.svelte";
+import Cookies from '$lib/components/banner/Cookies.svelte';
 import Footer from "$lib/components/footer/Footer.svelte";
 import ReturnButton from "$lib/components/misc/button/ReturnButton.svelte";
 import { onMount } from 'svelte';
 import { afterNavigate, invalidateAll } from '$app/navigation';
+import { browser } from '$app/environment';
 
 export let data;
 
 $: url = $page.url.pathname;
 
 $: admin = data.authorized;
+
+$: showCookies = browser && !localStorage.getItem('hide-cookies');
 
 const warn = console.warn;
 console.warn = (...args) => {
@@ -49,5 +53,9 @@ afterNavigate(() => {
 <slot/>
     {#if $page.params.id}
 <ReturnButton />
+    {/if}
+
+    {#if (showCookies)}
+<Cookies />
     {/if}
 <Footer />
