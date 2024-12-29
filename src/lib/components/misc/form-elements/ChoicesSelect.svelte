f4310629403f81bb101d4a07e4b5227b943b9a2c
whatevernumber <the_lost_number@mail.ru>
2024-12-29 18:31:33 +0300

    DOMIK-1 | Fix import

--- src/lib/components/misc/form-elements/ChoicesSelect.svelte
+++ src/lib/components/misc/form-elements/ChoicesSelect.svelte
@@ -1,5 +1,5 @@
 <script>
-	import "/choices.js/public/assets/styles/choices.css";
+	import "choices.js/public/assets/styles/choices.css";
 	import { onMount } from 'svelte';
 
 	export let fieldName = 'select';
